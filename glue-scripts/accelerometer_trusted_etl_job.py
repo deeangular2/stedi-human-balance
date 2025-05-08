00c673f7d27ce484a0cb023baa4bce367e972592
deeangular2 <deirdrethompson2015@gmail.com>
2025-05-08 13:21:28 -0400

    Add files via upload

---
+++ glue-scripts/accelerometer_trusted_etl_job.py
@@ -0,0 +1,50 @@
+import sys
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.job import Job
+
+# Get job name from arguments
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+# Initialize Glue job
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# 1. Load raw accelerometer data from the landing zone
+accelerometer_landing = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="json",
+    connection_options={"paths": ["s3://stedi-project-userthompson/accelerometer_landing/"]}
+)
+
+# 2. Load trusted customer data (already filtered for consent)
+customer_trusted = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="json",
+    connection_options={"paths": ["s3://stedi-project-userthompson/trusted/customer/"]}
+)
+
+# 3. Join on accelerometer.user = customer.email
+joined_data = Join.apply(
+    frame1=accelerometer_landing,
+    frame2=customer_trusted,
+    keys1=["user"],
+    keys2=["email"]
+)
+
+# 4. Write the joined (cleaned) data to the trusted/accelerometer folder
+glueContext.write_dynamic_frame.from_options(
+    frame=joined_data,
+    connection_type="s3",
+    format="json",
+    connection_options={"path": "s3://stedi-project-userthompson/trusted/accelerometer/"}
+)
+
+# 5. Commit the job
+job.commit()
+
---
+++ glue-scripts/customers_curated_job.py
@@ -0,0 +1,58 @@
+import sys
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+
+# Job setup
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# 1. Load customer trusted data
+customer_trusted = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="json",
+    connection_options={"paths": ["s3://stedi-project-userthompson/trusted/customer/"]}
+)
+
+# 2. Load accelerometer trusted data
+accelerometer_trusted = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="json",
+    connection_options={"paths": ["s3://stedi-project-userthompson/trusted/accelerometer/"]}
+)
+
+# Debug record counts
+print("Customer Trusted Count:", customer_trusted.count())
+print("Accelerometer Trusted Count:", accelerometer_trusted.count())
+
+# 3. Join on email = user
+curated_customers = Join.apply(
+    frame1=customer_trusted,
+    frame2=accelerometer_trusted,
+    keys1=["email"],
+    keys2=["user"]
+)
+
+print("Joined Curated Customer Count:", curated_customers.count())
+
+# 4. Drop duplicates using Spark DataFrame
+curated_customers_df = curated_customers.toDF().dropDuplicates(["email"])
+curated_customers = DynamicFrame.fromDF(curated_customers_df, glueContext, "curated_customers")
+
+# 5. Write output to curated/customers/
+glueContext.write_dynamic_frame.from_options(
+    frame=curated_customers,
+    connection_type="s3",
+    format="json",
+    connection_options={"path": "s3://stedi-project-userthompson/curated/customers/"}
+)
+
+# Complete the job
+job.commit()
