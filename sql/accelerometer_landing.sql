3c57f0582a159496f8a3419983a9120fc0377971
deeangular2 <deirdrethompson2015@gmail.com>
2025-05-09 14:39:54 -0400

    Add files via upload

---
+++ sql/accelerometer_landing.sql
@@ -0,0 +1,10 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS accelerometer_landing (
+  user STRING,
+  timestamp BIGINT,
+  x FLOAT,
+  y FLOAT,
+  z FLOAT
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS TEXTFILE
+LOCATION 's3://stedi-project-userthompson/accelerometer_landing/';
---
+++ sql/customer_landing.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS customer_landing (
+  customerName STRING,
+  email STRING,
+  phone STRING,
+  serialNumber STRING,
+  shareWithResearchAsOfDate BIGINT,
+  shareWithPublicAsOfDate BIGINT,
+  shareWithFriendsAsOfDate BIGINT,
+  registrationDate BIGINT,
+  birthDay STRING,
+  lastUpdateDate BIGINT
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS TEXTFILE
+LOCATION 's3://stedi-project-userthompson/customer_landing/';
