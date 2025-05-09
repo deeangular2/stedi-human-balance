c2b52207abf234c3761af35dbbcb13dc01434b31
deeangular2 <deirdrethompson2015@gmail.com>
2025-05-09 14:27:27 -0400

    Delete sql/StepTrainerLandingTable.sql

--- sql/StepTrainerLandingTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS step_trainer_landing (
-  sensorReadingTime BIGINT,
-  serialNumber STRING
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-STORED AS TEXTFILE
-LOCATION 's3://stedi-project-userthompson/step_trainer_landing/';
